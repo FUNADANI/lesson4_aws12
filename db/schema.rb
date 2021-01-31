dfc599df7524b3916f6f0d76448236b2823f42ce
FUNADANI <ryanmen9372@gmail.com>
2021-01-31 13:58:55 +0900

    MYSQL add

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2021_01_04_082132) do
 
-  create_table "articles", force: :cascade do |t|
+  create_table "articles", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.text "text"
     t.datetime "created_at", precision: 6, null: false
